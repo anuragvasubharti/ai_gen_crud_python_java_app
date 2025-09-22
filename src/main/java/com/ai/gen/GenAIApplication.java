47ed0dce5adb5406b68d0351147833b06cc71acc
anuragvasubharti <anuragvasubharti@gmail.com>
2025-09-22 22:07:53 +0530

    Added

---
+++ src/main/java/com/ai/gen/GenAIApplication.java
@@ -0,0 +1,11 @@
+package com.ai.gen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenAIApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GenAIApplication.class, args);
+	}
+}
